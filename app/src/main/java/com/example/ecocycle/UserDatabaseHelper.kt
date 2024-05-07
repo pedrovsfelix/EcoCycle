0f57954545a2f5b544f995ba478e966f7a3480e8
pedrovsfelix <pedrovitorsf@gmail.com>
2024-05-06 23:09:26 -0300

    feat: create conection sqlite.

---
+++ app/src/main/java/com/example/ecocycle/UserDatabaseHelper.kt
@@ -0,0 +1,40 @@
+package com.example.ecocycle
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class UserDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "register.db"
+        const val TABLE_NAME = "users"
+        const val COLUMN_USERNAME = "username"
+        const val COLUMN_EMAIL = "email"
+        const val COLUMN_PASSWORD = "password"
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        val createTable = "CREATE TABLE $TABLE_NAME ($COLUMN_USERNAME TEXT PRIMARY KEY, $COLUMN_EMAIL TEXT, $COLUMN_PASSWORD TEXT)"
+        db.execSQL(createTable)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+
+    fun insertUser(username: String, email: String, password: String): Boolean {
+        val db = writableDatabase
+        val contentValues = ContentValues().apply {
+            put(COLUMN_USERNAME, username)
+            put(COLUMN_EMAIL, email)
+            put(COLUMN_PASSWORD, password)
+        }
+        val result = db.insert(TABLE_NAME, null, contentValues)
+        db.close()
+        return result != -1L
+    }
+}
